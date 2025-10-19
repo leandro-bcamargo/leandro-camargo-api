f69ec9167bd1437f98341426ad54b99aef53e6af
Leandro Camargo <leandro.bcamargo@gmail.com>
2025-10-19 00:02:38 -0300

    Commit inicial

---
+++ src/main/java/br/edu/infnet/leandro_camargo_api/LeandroCamargoApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.leandro_camargo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeandroCamargoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeandroCamargoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/leandro_camargo_api/LeandroCamargoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.leandro_camargo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeandroCamargoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
